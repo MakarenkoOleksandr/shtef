fec698daa48a118827e90b9bd604271701289650
MakarenkoOleksandr <majop11111@gmail.com>
2024-06-24 16:05:59 +0200

    update

--- src/components/sections/main/Main-topsale.js
+++ src/components/sections/main/Main-topsale.js
@@ -30,12 +30,6 @@ const TopSale = () => {
             <p className="topsale__price">999$</p>
             <button className="topsale__btn">Add to cart</button>
           </div>
-          <div className="main__topsale-item topsale">
-            <img className="topsale__img" src={img} alt="img"></img>
-            <h3 className="topsale__model">Product Name</h3>
-            <p className="topsale__price">999$</p>
-            <button className="topsale__btn">Add to cart</button>
-          </div>
         </div>
       </div>
     </section>
