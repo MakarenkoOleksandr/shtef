3dc159758c041616415eee406ed51bb6ae73180f
MakarenkoOleksandr <majop11111@gmail.com>
2024-06-24 17:17:49 +0200

    added lang button

--- src/components/sections/header/Header-actions.js
+++ src/components/sections/header/Header-actions.js
@@ -1,7 +1,26 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCartShopping,
+  faGlobe,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import ua from "../../../img/ua.gif";
+import pl from "../../../img/pl.gif";
 
 const HeaderActions = () => {
+  const [language, setLanguage] = useState("UA");
+  const [fade, setFade] = useState(false);
+
+  const handleChangeLanguage = () => {
+    setFade(true);
+    setTimeout(() => {
+      setLanguage((prevLanguage) => (prevLanguage === "UA" ? "PL" : "UA"));
+      setFade(false);
+    }, 300);
+  };
+
+  console.log(language);
   return (
     <div className="header__actions-wrap">
       <div className="header__action">
@@ -18,6 +37,12 @@ const HeaderActions = () => {
           icon={faCartShopping}
           size="2xl"
           style={{ color: "black" }}
+        />
+      </div>
+      <div className="header__action" onClick={handleChangeLanguage}>
+        <img
+          className={`header__action-langimg ${fade ? "fade-out" : ""}`}
+          src={language === "UA" ? ua : pl}
         />
       </div>
     </div>
