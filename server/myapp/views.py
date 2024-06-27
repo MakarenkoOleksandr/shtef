b84aba9a83e64e7a5e9e60da81a8c533261ada39
MakarenkoOleksandr <majop11111@gmail.com>
2024-06-27 15:24:54 +0200

    update

--- server/myapp/views.py
+++ server/myapp/views.py
@@ -1,7 +1,14 @@
 from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.generic import View
 from myapp.models import Navigation
 
 
 def get_data(request):
     data = list(Navigation.objects.all().values())
     return JsonResponse(data, safe=False)
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
--- server/server/urls.py
+++ server/server/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from myapp.views import get_data
+from myapp.views import get_data, IndexView
 
 urlpatterns = [
     path('api/getNav/', get_data),
+    path('', IndexView.as_view(), name='index'),
+    path('<path:path>', IndexView.as_view(), name='index'),
 ]
