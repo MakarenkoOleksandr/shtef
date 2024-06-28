2f802e04f0054e8e5de641abcb084dd87b7f7624
MakarenkoOleksandr <majop11111@gmail.com>
2024-06-28 14:41:10 +0200

    upt

--- server/app.py
+++ server/app.py
@@ -1,14 +1,14 @@
 from flask import Flask
-from config import Config
+from config import DB
 from models import db
 from routes import api_bp
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(DB)
 
 db.init_app(app)
 
 app.register_blueprint(api_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- server/config.py
+++ server/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class Config:
+class DB:
     SQLALCHEMY_DATABASE_URI = os.getenv('DB')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
