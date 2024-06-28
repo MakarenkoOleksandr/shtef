ee1d21a0edc9abc9950845d94c93ddc91c09b70f
MakarenkoOleksandr <majop11111@gmail.com>
2024-06-28 15:38:30 +0200

    upd

--- server/routes.py
+++ server/routes.py
@@ -7,7 +7,9 @@ api_bp = Blueprint('api', __name__)
 @api_bp.route('/api/getNav', methods=['GET'])
 def get_navigation():
     try:
+        print("Trying to fetch navigation items...")
         navigations = Navigation.query.all()
+        print("Successfully fetched navigations:", navigations)
         return jsonify([navigation.to_dict() for navigation in navigations])
     except Exception as e:
         return jsonify({"error": str(e)}), 500
