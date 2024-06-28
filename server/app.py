d87e3376663c12c12cccfd790f000e17df5e3d94
MakarenkoOleksandr <majop11111@gmail.com>
2024-06-28 14:47:01 +0200

    upd

--- server/app.py
+++ server/app.py
@@ -1,13 +1,17 @@
+import os
+from dotenv import load_dotenv
+
 from flask import Flask
 from config import DB
 from models import db
 from routes import api_bp
 
+load_dotenv()
+
 app = Flask(__name__)
 app.config.from_object(DB)
 
 db.init_app(app)
-
 app.register_blueprint(api_bp)
 
 if __name__ == '__main__':
