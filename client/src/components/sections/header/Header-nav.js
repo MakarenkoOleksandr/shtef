b961d7714147dfecb2ea12f424cd9aa9feb8a37b
MakarenkoOleksandr <majop11111@gmail.com>
2024-06-28 16:21:05 +0200

    upd

--- client/src/components/sections/header/Header-nav.js
+++ client/src/components/sections/header/Header-nav.js
@@ -2,7 +2,10 @@ import { NavLink } from "react-router-dom";
 import useGetNav from "../../api/useGetNav";
 
 const HeaderNav = ({ language, fade }) => {
-  const navBtns = useGetNav("/api/getNav", language);
+  const navBtns = useGetNav(
+    "https://shnyrki-552c68a5b166.herokuapp.com/api/getNav",
+    language
+  );
 
   return (
     <nav className="header__nav">
